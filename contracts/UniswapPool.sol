65737dacc878183d0c8d9d07a9282ae6e2dd4612
Luke Van Seters <lukevanseters@gmail.com>
2021-05-25 07:42:04 -0400

    Add basic deposit test

--- contracts/UniswapPool.sol
+++ contracts/UniswapPool.sol
@@ -3,48 +3,86 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "./UniswapLPToken.sol";
-
-// import "./Math.sol";
+import "./Math.sol";
 
 contract UniswapPool {
     using SafeERC20 for IERC20Metadata;
     using Math for uint256;
+    using SafeMath for uint256;
 
-    IERC20Metadata private firstToken;
-    IERC20Metadata private secondToken;
+    IERC20Metadata public firstToken;
+    IERC20Metadata public secondToken;
     UniswapLPToken public lpToken;
 
-    constructor(address firstTokenAddress, address secondTokenAddress) {
+    // TODO - events
+
+    constructor(IERC20Metadata firstTokenAddress, IERC20Metadata secondTokenAddress) {
         firstToken = IERC20Metadata(firstTokenAddress);
         secondToken = IERC20Metadata(secondTokenAddress);
-        lpToken = _createLPToken(firstTokenAddress, secondTokenAddress);
+        lpToken = _createLPToken();
     }
 
-    function _createLPToken(address firstTokenAddress, address secondTokenAddress) internal returns (UniswapLPToken) {
+    function _createLPToken() private returns (UniswapLPToken) {
         string memory name =
             string(abi.encodePacked("Uniswap ", firstToken.name(), " + ", secondToken.name(), " LP Token"));
         string memory symbol = string(abi.encodePacked("uni", firstToken.symbol(), "+", secondToken.symbol()));
-        return new UniswapLPToken(name, symbol, firstTokenAddress, secondTokenAddress);
+        return new UniswapLPToken(name, symbol, address(firstToken), address(secondToken));
     }
 
-    // function deposit(
-    //     uint256 firstTokenAmount,
-    //     uint256 secondTokenAmount
-    // ) external {
-    //     requre(firstTokenAmount > 0, "First token amount must be greater than 0");
-    //     requre(secondTokenAmount > 0, "Second token amount must be greater than 0");
+    function addLiquidity(
+        uint256 firstTokenAmount,
+        uint256 secondTokenAmount
+    ) external {
+        _transferTokensToPool(msg.sender, firstTokenAmount, secondTokenAmount);
+        uint256 lpTokenAmount = _calculateLPTokenAmountToMint(firstTokenAmount, secondTokenAmount);
+        lpToken.mint(msg.sender, lpTokenAmount);
+    }
 
-    //     firstToken = IERC20(firstTokenAddress);
-    //     secondToken = IERC20(secondTokenAddress);
+    function _transferTokensToPool(
+        address depositor,
+        uint256 firstTokenAmount,
+        uint256 secondTokenAmount
+    ) internal {
+        require(firstTokenAmount > 0, "First token amount must be greater than 0");
+        require(secondTokenAmount > 0, "Second token amount must be greater than 0");
 
-    //     require(firstToken.allowance(msg.sender, address(this)) >= firstTokenAmount, "Not enough allowed first tokens");
-    //     require(secondToken.allowance(msg.sender, address(this)) >= secondTokenAmount, "Not enough allowed second tokens");
+        require(firstToken.allowance(depositor, address(this)) >= firstTokenAmount, "Not enough allowed first tokens");
+        require(
+            secondToken.allowance(depositor, address(this)) >= secondTokenAmount,
+            "Not enough allowed second tokens"
+        );
 
-    //     firstToken.safeTransferFrom(msg.sender, address(this), firstTokenAmount);
-    //     secondToken.safeTransferFrom(msg.sender, address(this), secondTokenAmount);
+        firstToken.safeTransferFrom(depositor, address(this), firstTokenAmount);
+        secondToken.safeTransferFrom(depositor, address(this), secondTokenAmount);
+    }
 
-    //     uint256 lpTokenAmount = (firstTokenAmount * secondTokenAmount).sqrt();
-    //     lpToken.call(abi.encodeWithSignature("mint(address, uint256)", msg.sender, lpTokenAmount));
-    // }
+    function _calculateLPTokenAmountToMint(
+        uint256 firstTokenAmount,
+        uint256 secondTokenAmount
+    ) internal view returns (uint256) {
+        uint256 lpTokenSupply = lpToken.totalSupply();
+        if (lpTokenSupply == 0) {
+            // TODO - how is this handled for irrational results?
+            return (firstTokenAmount * secondTokenAmount).sqrt();
+        } else {
+            uint256 totalFirstTokenInPool = _getFirstTokenBalance();
+            uint256 totalSecondTokenInPool = _getSecondAmountBalance();
+
+            uint256 totalValueInPoolAsFirstToken = totalFirstTokenInPool.mul(2);
+            uint256 secondTokenAmountAddedAsFirstToken = secondTokenAmount.mul(totalFirstTokenInPool).div(totalSecondTokenInPool);
+            uint256 totalValueAddedAsFirstToken = firstTokenAmount.add(secondTokenAmountAddedAsFirstToken);
+
+            return lpTokenSupply.div((totalValueInPoolAsFirstToken.div(totalValueAddedAsFirstToken)).sub(1));
+        }
+    }
+
+    function _getFirstTokenBalance() private view returns (uint256) {
+        return firstToken.balanceOf(address(this));
+    }
+
+    function _getSecondAmountBalance() private view returns (uint256) {
+        return secondToken.balanceOf(address(this));
+    }
 }
--- test/UniswapPool.ts
+++ test/UniswapPool.ts
@@ -9,7 +9,6 @@ const { deployContract } = hre.waffle;
 export function shouldBehaveLikeUniswapPool(): void {
   describe("UniswapPool", function () {
     beforeEach(async function () {
-      const lpTokenArtifact: Artifact = await hre.artifacts.readArtifact("UniswapLPToken");
       const poolArtifact: Artifact = await hre.artifacts.readArtifact("UniswapPool");
       this.pool = <UniswapPool>await deployContract(
         this.signers.admin,
@@ -25,6 +24,29 @@ export function shouldBehaveLikeUniswapPool(): void {
 
     it("should assign the contract address as the LP token owner", async function () {
       expect(await this.lpToken.owner()).to.equal(this.pool.address);
+    });
+
+    it("should exchange liquidity for LP tokens", async function () {
+      const aliceASupply = 100;
+      const aliceBSupply = 200;
+
+      const aliceADeposit = 10;
+      const aliceBDeposit = 40;
+
+      await this.tokenA.connect(this.signers.admin).mint(this.signers.alice.address, aliceASupply);
+      await this.tokenB.connect(this.signers.admin).mint(this.signers.alice.address, aliceBSupply);
+
+      await this.tokenA.connect(this.signers.alice).approve(this.pool.address, aliceADeposit);
+      await this.tokenB.connect(this.signers.alice).approve(this.pool.address, aliceBDeposit);
+
+      await this.pool.connect(this.signers.alice).addLiquidity(aliceADeposit, aliceBDeposit);
+
+      const expectedLPTokenSupply = Math.sqrt(aliceADeposit * aliceBDeposit);
+      expect(await this.lpToken.totalSupply()).to.equal(expectedLPTokenSupply);
+      expect(await this.lpToken.balanceOf(this.signers.alice.address)).to.equal(expectedLPTokenSupply);
+
+      expect(await this.tokenA.balanceOf(this.pool.address)).to.equal(aliceADeposit);
+      expect(await this.tokenB.balanceOf(this.pool.address)).to.equal(aliceBDeposit);
     });
   });
 };
